4e00fbbea9afa90be1f3c2e1c403cc5014bcae21
janak mahara <maharajanak057@gmail.com>
2022-12-14 16:44:58 +0545

    added js file

---
+++ demo.js
@@ -0,0 +1,18 @@
+
+// let person1 = {
+//     MyName : "Janak Mahara",
+//     OtherName  : "Ram Shah"
+//   };
+
+// if(person1.MyName == "Janak mahara")
+// {
+
+//   console.log( "my name is: "+person1["MyName"]);
+// }
+// else
+// {
+//   console.log("Other's name is: "+person1["OtherName"]);
+// }
+
+
+   
